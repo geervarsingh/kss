4cdf9a67b27314e77b63421fddd93414adc98adb
geervarsingh <86580425+geervarsingh@users.noreply.github.com>
2021-06-27 18:44:09 -0700

    Add files via upload

---
+++ JeetPakki/app/src/main/java/bhobhli/kss/jeetpakki/ZeetPakkiTutorial/MainActivity.java
@@ -0,0 +1,27 @@
+package bhobhli.kss.jeetpakki.ZeetPakkiTutorial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+import bhobhli.kss.jeetpakki.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
